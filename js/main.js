612e35b3496a271fc51971c08fbba74af0efcdd5
psdstudio <71070672+psdstudio@users.noreply.github.com>
2020-10-29 17:06:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function(){
+	$(".mainSlider").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		prevArrow:'<div class="sliderBtn prevBtn">이전</div>',
+		nextArrow:'<div class="sliderBtn nextBtn">다음</div>'
+      });
+})*/
